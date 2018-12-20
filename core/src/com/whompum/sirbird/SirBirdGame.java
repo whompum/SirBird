8c1128ddc9191a226a5f1ce3738c0744c0c806af
Bryan <whompum101@gmail.com>
2018-12-20 10:57:21 -0500

    Initialize and dispose of game assets

--- core/src/com/whompum/sirbird/SirBirdGame.java
+++ core/src/com/whompum/sirbird/SirBirdGame.java
@@ -3,12 +3,20 @@ package com.whompum.sirbird;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.whompum.sirbird.Contexts.PlayContext;
+import com.whompum.sirbird.GameItemLoaders.AssetLoader;
 
 public class SirBirdGame extends Game {
 
     @Override
     public void create() {
         Gdx.app.log( "ZBGame", "created" );
+        AssetLoader.load();
         setScreen( new PlayContext() );
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
     }
 }
