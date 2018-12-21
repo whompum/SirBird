cbd25151ed9b3d285900a36b3bccb9a8bc630248
Bryan <whompum101@gmail.com>
2018-12-21 11:30:33 -0500

    Rm vector wrappers around simple maths; Adjust physics sensitivity; Add progressive bird rotation on fall

--- core/src/com/whompum/sirbird/GameContextModels/SirBird.java
+++ core/src/com/whompum/sirbird/GameContextModels/SirBird.java
@@ -1,64 +1,109 @@
 package com.whompum.sirbird.GameContextModels;
 
 import com.badlogic.gdx.math.Vector2;
+import com.whompum.sirbird.Logger;
 
 /**
  * The modeled state of the Bird the user interacts with
  */
 public class SirBird {
 
-    public static final int TERMINAL_VELOCITY = 200;
+    public static final int TERMINAL_VELOCITY = 7; //How many units to fall per update,
+    public static final int VERTICAL_JUMP_UNITS = -4; //How many units to raise per update
+
+    public static final int ROTATION_ANGLE = 90;
 
     private int width, height;
 
     //Vector2 objects will simply hold X,Y pairs in our situation
-    private Vector2 currPos, /*How fast we're currently falling*/ descendingVelocity, acceleration;
+    private Vector2 currPos;
 
-    private float vectorAngle;
+    private float currentVelocity;
 
-    public SirBird(final int width, final int height, final int startingXPos, final int startingYPos){
+    private float currentAngle;
+
+    private int contextHeight;
+
+    public SirBird(final int width, final int height, final int startingXPos, final int startingYPos, final int contextHeight){
 
         this.width = width;
         this.height = height;
 
         currPos = new Vector2( startingXPos, startingYPos );
-        descendingVelocity = new Vector2();
-        acceleration = new Vector2( 0, 560 );
 
+        this.contextHeight = contextHeight;
     }
 
     /**
-     * Update the current position and velocity of this entity
-     * by utilizing the `deltaFrameUpdate` parameter.
+     * Updates the current position of SirBird based on the current velocity
      *
-     * Updating the {@link Vector2} objects like this will produce a standardizing
-     * effect on our bird movement. Basically, if the deltaFrameUpdate was 2X as long as last time
-     * we'll just move 2X at far, thus giving us a FPS-independent movement.
+     * Basically how it works is we take the deltaFrameUpdate (millis since last update)
+     * and find out how much our velocity should increase / decrease by scaling the DECELERATION_CIEL by it.
+     *
+     * We then append that value to the currVelocity to find our new fall speed (in units)
+     *
+     * After we find our new velocity we then scale it by deltaFrameUpdate to find how many units the bird should move
+     *
+     * Then simply append that result to our current position
      *
      * @param deltaFrameUpdate
      */
     public void updatePos(final float deltaFrameUpdate){
+        setCurrentVelocity( getDeltaFallSpeed( deltaFrameUpdate ) );
+        currPos.add( 0, currentVelocity );
 
-        final Vector2 scaledAcceleration = scale( acceleration, deltaFrameUpdate );
-
-        descendingVelocity.add( scaledAcceleration );
-
-        if( descendingVelocity.y > TERMINAL_VELOCITY ) //If velocity is greater than the terminal velocity
-            descendingVelocity.y = TERMINAL_VELOCITY;
-
-        final Vector2 scaledVelocity = scale( descendingVelocity, deltaFrameUpdate );
-
-        currPos.add( scaledVelocity );
+        if( isTouchingCiel() ){
+            currPos.y = 0;
+            restartVelocity();
+        } else if( isTouchingFloor() ){
+            currPos.y = contextHeight - height;
+            currentVelocity = 0F;
+        }
 
     }
 
-    private Vector2 scale(final Vector2 sbj, final float delta){
-        //TODO fix allocations during each screen rendering, from the Vector2#cpy() method
-        return sbj.cpy().scl( delta );
+    private float getDeltaFallSpeed(final float deltaFrameUpdate){
+        return TERMINAL_VELOCITY * deltaFrameUpdate;
+    }
+
+    private void setCurrentVelocity(final float deltaFallSpeed){
+
+        this.currentVelocity += deltaFallSpeed;
+
+        if( currentVelocity > TERMINAL_VELOCITY )
+            currentVelocity = TERMINAL_VELOCITY;
+
+        if( isDescending() ){
+
+            currentAngle += (ROTATION_ANGLE * deltaFallSpeed) * 0.5F; //Scale by half to slow down the rotation
+
+            if( currentAngle >= ROTATION_ANGLE )
+                currentAngle = ROTATION_ANGLE;
+
+        }else if( currentAngle != 0 )
+            currentAngle = 0;
+
     }
 
     public void onClick() {
-        descendingVelocity.y = -140;
+       currentVelocity = VERTICAL_JUMP_UNITS;
+       currentAngle = -90;
+    }
+
+    private boolean isTouchingCiel(){
+        return currPos.y <= 0;
+    }
+
+    private boolean isTouchingFloor(){
+        return (currPos.y + height) >= contextHeight;
+    }
+
+    public boolean isDescending(){
+        return currentVelocity > 0;
+    }
+
+    private void restartVelocity(){
+        currentVelocity = 0;
     }
 
     public float getX() {
@@ -78,7 +123,7 @@ public class SirBird {
     }
 
     public float getRotation() {
-        return vectorAngle;
+        return currentAngle;
     }
 
 }
