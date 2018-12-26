f77f79f26088a095a160b06f6a4194d34e1c4361
Bryan <whompum101@gmail.com>
2018-12-26 10:24:11 -0500

    Draw pipes / bird

--- core/src/com/whompum/sirbird/Renderer/GameRenderer.java
+++ core/src/com/whompum/sirbird/Renderer/GameRenderer.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.whompum.sirbird.GameContextModels.Pipes.Pipe;
+import com.whompum.sirbird.GameContextModels.Pipes.PipeGroup;
+import com.whompum.sirbird.GameContextModels.Pipes.PipeManager;
 import com.whompum.sirbird.GameContextModels.SirBird;
 import com.whompum.sirbird.GameItemLoaders.AssetLoader;
 import com.whompum.sirbird.GameWorld;
-import org.w3c.dom.Text;
 
 /**
  * Helper class to render game objects such as SirBird or the Castles
@@ -27,10 +29,18 @@ public class GameRenderer implements RendererHelper{
 
     private SirBird bird;
 
+    private PipeManager pipeManager;
+
     private TextureRegion stableBird = AssetLoader.birdStable;
     private Animation<TextureRegion> birdAnim = AssetLoader.birdAnimation;
     private TextureRegion grass = AssetLoader.grass;
     private TextureRegion background = AssetLoader.background;
+
+    private TextureRegion pipeBody = AssetLoader.pipeBody;
+    private TextureRegion pipeHeadDown = AssetLoader.pipeHeadDown;
+    private TextureRegion pipeHeadUp = AssetLoader.pipeHeadUp;
+
+    private float runtime;
 
     public GameRenderer(final GameWorld world, final int height, final int cY){
         this.world = world;
@@ -49,13 +59,21 @@ public class GameRenderer implements RendererHelper{
         this.cY = cY;
 
         bird = world.getBird();
+
+        pipeManager = new PipeManager( 136, cY+66 );
+
     }
 
     @Override
-    public void render(final float runTime){
+    public void render(final float deltaFps){
+
+        runtime += deltaFps;
+
         //Drawing black prevents view flickering
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        pipeManager.update( deltaFps );
 
         shapeRenderer.begin( ShapeRenderer.ShapeType.Filled );
 
@@ -76,16 +94,33 @@ public class GameRenderer implements RendererHelper{
         // This is good for performance when drawing images that do not require
         // transparency.
         batcher.disableBlending();
-        batcher.draw(AssetLoader.background, 0, cY + 23, 136, 43);
+        batcher.draw( background, 0, cY + 23, 136, 43);
 
         // The bird needs transparency, so we enable that again.
         batcher.enableBlending();
 
-        drawSirBird( batcher, ( bird.isDescending() ) ? AssetLoader.birdStable : AssetLoader.birdAnimation.getKeyFrame( runTime ) );
+        drawSirBird( batcher, ( bird.isDescending() ) ? stableBird : birdAnim.getKeyFrame( runtime ) );
+
+        for( PipeGroup p: pipeManager.getPipes() )
+            for( Pipe pipe: p.getPipes() ){
+
+            if( pipe.getVisibilityState().equals( Pipe.VISIBILITY_STATE.STATE_INVISIBLE ) )
+                continue;
+
+            final boolean baseFloor = pipe.isBaseFloor();
+
+            final TextureRegion head = (baseFloor) ? pipeHeadDown : pipeHeadUp;
+            final int pipeY = (baseFloor) ? (int)pipe.getTop() : 0;
+            final int headY = (baseFloor) ? (int)pipe.getTop() : (int)pipe.getBottom() - pipe.getHeadHeight();
+
+            final int pipeHeight = pipe.getHeight();
+
+            batcher.draw( pipeBody, pipe.getPipeBodyStart(), pipeY, pipe.getPipeBodyWidth(), pipeHeight );
+            batcher.draw( head, pipe.getStart(), headY, pipe.getHeadWidth(), pipe.getHeadHeight() );
+        }
 
         // End SpriteBatch
         batcher.end();
-
     }
 
     private void drawSirBird( final SpriteBatch batcher, final TextureRegion region){
